d2015fba530af8a476eaa691f9fa542b9b7e5619
Sarah Allen <sarah.sra1@gmail.com>
2013-09-23 12:21:29 -0700

    Added texture selection jquery.

--- js/card-o-matic.js
+++ js/card-o-matic.js
@@ -1,4 +1,10 @@
 $(".colors").click(function() {
   var color_picked = $(this).css('background-color');
   $("#canvas").css('background-color', color_picked);
+  $(".textures").css('background-color', color_picked);
+});
+
+$('.textures').click(function() {
+  var texture_picked = $(this).css('background-image');
+  $("#canvas").css('background-image', texture_picked);
 });
